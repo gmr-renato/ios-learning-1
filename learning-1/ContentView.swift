816d9e49f48221045ec8041ced51b758ab8cf2af
Renato Marinho Gonçalves <gmr.renato@gmail.com>
2022-12-25 16:36:22 -0300

    Initial Commit

---
+++ learning-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  learning-1
+//
+//  Created by Renato M. Gonçalves on 25/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning-1/learning_1App.swift
@@ -0,0 +1,17 @@
+//
+//  learning_1App.swift
+//  learning-1
+//
+//  Created by Renato M. Gonçalves on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct learning_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
